843b8f18e6a386c05fc6bfc3f0289f7fd591fadf
Snowmor2399 <moraleslp@hotmail.com>
2026-01-30 22:00:58 +0100

    Dockerfile actualización commit servidor

--- src/main/java/com/morales/appserver/security/JwtUtil.java
+++ src/main/java/com/morales/appserver/security/JwtUtil.java
@@ -2,9 +2,12 @@ package com.morales.appserver.security;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
 import java.util.Date;
 
 @Component
@@ -12,12 +15,18 @@ public class JwtUtil {
     @Value("${JWT_LLAVE}")
     private String secretKey;
 
+    private Key getSigningKey() {
+        // Esto convierte tu frase de Render en una llave segura sin importar los caracteres
+        byte[] keyBytes = secretKey.getBytes(StandardCharsets.UTF_8);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
     public String generateToken(String email) {
         return Jwts.builder()
                 .setSubject(email)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
-                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
 }
